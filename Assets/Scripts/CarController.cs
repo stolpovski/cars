31f632d3f0412a2b9c66a5c50432bebac4588f4d
stolpovski <stolpovski@gmail.com>
2023-08-25 00:44:39 +0300

    update

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -27,6 +27,7 @@ public class CarController : MonoBehaviour
     private AudioSource _engineStart;
     private AudioSource _engineStop;
     AudioSource _horn;
+    AudioSource _brakes;
 
     private Controls _controls;
     private float _throttle;
@@ -34,9 +35,21 @@ public class CarController : MonoBehaviour
 
     private bool _engineRunning;
     Transform _spawn;
+    ParticleSystem _smoke;
+    GameObject _lights;
 
     private void Awake()
     {
+        _smoke = GameObject.Find("Smoke").GetComponent<ParticleSystem>();
+        /*GameObject go = GameObject.Find("Traffic_light_EU_red");
+        while (go)
+        {
+            go.GetComponent<MeshRenderer>().enabled = false;
+            go = GameObject.Find("Traffic_light_EU_red");
+        }*/
+        _lights = GameObject.Find("Lights");
+        _lights.SetActive(false);
+
         _spawn = GameObject.Find("Spawn")?.transform;
         Cursor.lockState = CursorLockMode.Locked;
         rb = GetComponent<Rigidbody>();
@@ -45,6 +58,7 @@ public class CarController : MonoBehaviour
         _engineStart = GameObject.Find("Start").GetComponent<AudioSource>();
         _engineStop = GameObject.Find("Stop").GetComponent<AudioSource>();
         _horn = GameObject.Find("Horn").GetComponent<AudioSource>();
+        //_brakes = GameObject.Find("Brakes").GetComponent<AudioSource>();
 
         _controls = new Controls();
         _controls.Car.Enable();
@@ -58,10 +72,17 @@ public class CarController : MonoBehaviour
         _controls.Car.Steer.performed += context => _steer = context.ReadValue<float>();
         _controls.Car.Steer.canceled += context => _steer = 0f;
 
-        _controls.Car.Brake.performed += context => currentBrakeForce = brakeForce;
+        _controls.Car.Brake.performed += Brake;
         _controls.Car.Brake.canceled += context => currentBrakeForce = 0f;
 
         _controls.Car.Horn.performed += Horn;
+        //_controls.Car.Horn.canceled += HornStop;
+    }
+
+    void Brake(InputAction.CallbackContext ctx)
+    {
+        currentBrakeForce = brakeForce;
+        //_brakes.Play();
     }
 
     void Respawn(InputAction.CallbackContext ctx)
@@ -73,7 +94,14 @@ public class CarController : MonoBehaviour
 
     void Horn(InputAction.CallbackContext ctx)
     {
+        
         _horn.Play();
+    }
+
+    void HornStop(InputAction.CallbackContext ctx)
+    {
+
+        _horn.Stop();
     }
 
     private void ToggleEngine(InputAction.CallbackContext ctx)
@@ -82,13 +110,17 @@ public class CarController : MonoBehaviour
 
         if (_engineRunning)
         {
+            _lights.SetActive(true);
             _engineStart.Play();
             _engine.Play();
+            _smoke.Play();
         }
         else
         {
+            _lights.SetActive(false);
             _engine.Stop();
             _engineStop.Play();
+            _smoke.Stop();
         }
     }
 
@@ -115,7 +147,7 @@ public class CarController : MonoBehaviour
         UpdateWheel(rearLeft, rearLeftTransform);
         UpdateWheel(rearRight, rearRightTransform);
 
-        _engine.pitch = 1 + rb.velocity.magnitude / 10;
+        _engine.pitch = 1 + rb.velocity.magnitude / 5;
     }
 
     void UpdateWheel(WheelCollider col, Transform trans)
